566c34da3bb53c18726d53159cadf7bdf7904e40
augustolj <augustolj@gmail.com>
2019-02-19 21:59:45 -0300

    Add files via upload

--- js.js
+++ js.js
@@ -29,18 +29,18 @@ function juego() {
   var eleccion = parseInt(prompt("Elije\n 1-Piedra\n 2-Papel\n 3-Tijera"));
   if (isNaN(eleccion)) {
     alert("Ingreso no valido");
-} else switch (eleccion) {
-  case 1:
-    alert("Papel!");
-    break;
-  case 2:
-    alert("Tijera!");
-    break;
-  case 3:
-    alert("Piedra!");
-    break;
-  default:
-    alert("Ingreso no valido");
-    break;
-}
+  } else switch (eleccion) {
+    case 1:
+      alert("Papel!");
+      break;
+    case 2:
+      alert("Tijera!");
+      break;
+    case 3:
+      alert("Piedra!");
+      break;
+    default:
+      alert("Ingreso no valido");
+      break;
+  }
 }
